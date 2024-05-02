461b69a7926a60a7867544bb5fc18c3c3222c1c5
loppam <lolade132@gmail.com>
2024-05-02 09:08:07 +0100

    Done

--- app/api/action/route.ts
+++ app/api/action/route.ts
@@ -1,23 +1,9 @@
-import { FrameRequest, getFrameMessage } from '@coinbase/onchainkit/frame';
+import { FrameRequest, getFrameMessage, getFrameHtmlResponse } from '@coinbase/onchainkit/frame';
 import { NextRequest, NextResponse } from 'next/server';
+import { NEXT_PUBLIC_URL } from '../../config';
 
 async function getResponse(req: NextRequest): Promise<NextResponse> {
-  return new NextResponse('Response from getResponse function', { status: 200 });
-}
-
-export async function POST(req: NextRequest): Promise<NextResponse> {
-  console.log('POST');
-  return getResponse(req);
-}
-
-export async function GET(req: NextRequest): Promise<NextResponse> {
-  console.log('GET');
-  return getResponse(req);
-}
-
-export const dynamic = 'force-dynamic';
-
-export default async function handler(req: NextRequest): Promise<NextResponse> {
+  console.log('getResponse');
   const body: FrameRequest = await req.json();
   const { isValid, message } = await getFrameMessage(body, { neynarApiKey: 'NEYNAR_ONCHAIN_KIT' });
 
@@ -25,5 +11,25 @@ export default async function handler(req: NextRequest): Promise<NextResponse> {
     return new NextResponse('Message not valid', { status: 500 });
   }
 
-  return NextResponse.json({ message: 'Hello from the frame route' }, { status: 200 });
+  // Get the current date and time
+  const now = new Date();
+  const date = now.toLocaleDateString();
+  const time = now.toLocaleTimeString();
+
+  // Format them as a string
+  const dateTime = `${date} ${time} UTC`;
+
+  return NextResponse.json({ message: dateTime }, { status: 200 });
 }
+
+export async function POST(req: NextRequest): Promise<Response> {
+  console.log('POST');
+  return getResponse(req);
+}
+
+export async function GET(req: NextRequest): Promise<Response> {
+  console.log('GET');
+  return getResponse(req);
+}
+
+export const dynamic = 'force-dynamic';
